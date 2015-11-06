bb23576b75a25d2ac710479c09af401bc27ba7dc
Nathan <nyee32@gmail.com>
2015-11-05 23:15:23 -0800

    Added ability to set the text of button

--- app/src/main/java/com/example/nathan/tictacktoe_android/Game.java
+++ app/src/main/java/com/example/nathan/tictacktoe_android/Game.java
@@ -8,8 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class Game extends AppCompatActivity {
+    private playerTurn turn = playerTurn.turnO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,5 +41,17 @@ public class Game extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setText(View view) {
+        Button piece = (Button) view;
+
+        if (turn == playerTurn.turnX) {
+            piece.setText("X");
+            turn = playerTurn.turnO;
+        } else {
+            piece.setText("O");
+            turn = playerTurn.turnX;
+        }
     }
 }
---
+++ app/src/main/java/com/example/nathan/tictacktoe_android/playerTurn.java
@@ -0,0 +1,8 @@
+package com.example.nathan.tictacktoe_android;
+
+/**
+ * Created by Nathan on 11/5/2015.
+ */
+public enum playerTurn {
+    turnX, turnO
+}
